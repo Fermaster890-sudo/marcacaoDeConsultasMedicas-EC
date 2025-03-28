586cdbdc937e88cf3f26042ef34ebd720ec87dab
Fermaster890-sudo <ferperezine@gmail.com>
2025-03-28 20:25:29 -0300

    Inicializando projeto Expo - estruturas básicas

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Engenharia da Computação</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto: {
+    fontSize: 30,
+    color: '#FFFFFF',
+    fontWeight: 'bold'
+  }
+});
